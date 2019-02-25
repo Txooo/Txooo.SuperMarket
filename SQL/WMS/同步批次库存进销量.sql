d41ab6e18b07422fa017b5c8d186de407c9e061b
马发才 <43975111+open-mfc@users.noreply.github.com>
2019-02-25 13:45:31 +0800

    Update and rename 重新同步采购单批次库存进量.sql to 同步批次库存进销量.sql

--- SQL/WMS/同步批次库存进销量.sql
+++ SQL/WMS/同步批次库存进销量.sql
@@ -1,3 +1,4 @@
+--同步库存采购数量
 UPDATE dbo.vmall_bill_warehouse_inventory
 SET inventory_in = ss2.goods_count
 --SELECT wi.inventory_id,ss2.* 
@@ -20,3 +21,25 @@ FROM dbo.vmall_bill_warehouse_inventory AS wi
         ON ss2.batch_id = wi.batch_id
            AND wi.warehouse_id = ss2.into_warehouse_id
 WHERE wi.inventory_in <> ss2.goods_count;
+
+--同步库存销售数量
+UPDATE dbo.vmall_bill_warehouse_inventory
+SET inventory_out = wil.goods_count
+--SELECT wil.*,wi.inventory_out 
+FROM dbo.vmall_bill_warehouse_inventory AS wi
+    INNER JOIN
+    (
+        SELECT SUM(   CASE inventory_type
+                          WHEN 12 THEN
+                              -goods_count
+                          ELSE
+                              goods_count
+                      END
+                  ) goods_count,
+               inventory_from
+        FROM dbo.vmall_bill_warehouse_inventory_log
+        WHERE inventory_type IN ( 11, 12 )
+        GROUP BY inventory_from
+    ) wil
+        ON wi.inventory_id = wil.inventory_from
+WHERE wi.inventory_out <> wil.goods_count;
