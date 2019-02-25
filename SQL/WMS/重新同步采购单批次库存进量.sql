657f4b839a4f343ad117ac7e7a37822a7690f065
马发才 <43975111+open-mfc@users.noreply.github.com>
2019-02-25 13:32:23 +0800

    Update 重新同步采购单批次库存进量.sql

--- SQL/WMS/重新同步采购单批次库存进量.sql
+++ SQL/WMS/重新同步采购单批次库存进量.sql
@@ -1,14 +1,22 @@
-UPDATE dbo.vmall_bill_warehouse_inventory SET inventory_in=ss2.goods_count
+UPDATE dbo.vmall_bill_warehouse_inventory
+SET inventory_in = ss2.goods_count
 --SELECT wi.inventory_id,ss2.* 
 FROM dbo.vmall_bill_warehouse_inventory AS wi
-INNER JOIN (
-SELECT s.into_warehouse_id,SUM(sid2.goods_count) goods_count,sid2.batch_id 
-FROM dbo.vmall_bill_supply_into_detail sid2
-INNER JOIN dbo.vmall_bill_supply_into AS si
-ON si.into_id = sid2.into_id
-INNER JOIN dbo.vmall_bill_supply AS s
-ON s.bill_id = si.bill_id
-WHERE si.check_status=4 AND s.check_status=6
-GROUP BY s.into_warehouse_id, sid2.batch_id ) AS ss2
-ON ss2.batch_id = wi.batch_id AND wi.warehouse_id=ss2.into_warehouse_id
-WHERE wi.inventory_in<>ss2.goods_count
+    INNER JOIN
+    (
+        SELECT s.into_warehouse_id,
+               SUM(sid2.goods_count) goods_count,
+               sid2.batch_id
+        FROM dbo.vmall_bill_supply_into_detail sid2
+            INNER JOIN dbo.vmall_bill_supply_into AS si
+                ON si.into_id = sid2.into_id
+            INNER JOIN dbo.vmall_bill_supply AS s
+                ON s.bill_id = si.bill_id
+        WHERE si.check_status = 4
+              AND s.check_status = 6
+        GROUP BY s.into_warehouse_id,
+                 sid2.batch_id
+    ) AS ss2
+        ON ss2.batch_id = wi.batch_id
+           AND wi.warehouse_id = ss2.into_warehouse_id
+WHERE wi.inventory_in <> ss2.goods_count;
