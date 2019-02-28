f9d674e16a12bcea09b6e0f495a66460f92f4ab6
马发才 <43975111+open-mfc@users.noreply.github.com>
2019-02-28 15:44:14 +0800

    Create 手动关联人脸会员.sql

---
+++ SQL/UserCenter/手动关联人脸会员.sql
@@ -0,0 +1,21 @@
+UPDATE [dbo].[gzy_user_third] 
+SET user_id=b.new_face_id
+FROM dbo.gzy_user_third AS a 
+LEFT JOIN dbo.face_user_map AS b
+ON b.old_user_id = a.old_user_id
+
+UPDATE dbo.gzy_user_third SET com_id=b.com_id
+FROM dbo.gzy_user_third AS a 
+LEFT JOIN TxoooBrands.dbo.brand_index AS b
+ON a.brand_id=b.brand_id
+
+UPDATE dbo.gzy_user_member 
+SET user_id=b.user_id,com_id=b.com_id
+FROM dbo.gzy_user_member AS a 
+LEFT JOIN dbo.gzy_user_third AS b
+ON a.mobile=b.mobile AND a.brand_id=b.brand_id
+
+UPDATE dbo.gzy_user_third SET member_id=b.member_id
+FROM dbo.gzy_user_third AS a
+LEFT JOIN dbo.gzy_user_member AS b
+ON b.brand_id = a.brand_id AND b.user_id = a.user_id
