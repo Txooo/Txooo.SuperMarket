0ea678134d15dfb98d45750791d6a30f8d4503ef
马发才 <43975111+open-mfc@users.noreply.github.com>
2019-03-20 08:55:42 +0800

    Update 3、手动关联人脸会员.sql

--- SQL/UserCenter/3、手动关联人脸会员.sql
+++ SQL/UserCenter/3、手动关联人脸会员.sql
@@ -12,20 +12,20 @@ FROM dbo.gzy_user_third AS a
     LEFT JOIN TxoooBrands.dbo.brand_index AS b
         ON a.brand_id = b.brand_id;
 
---更新会员人脸和企业
-UPDATE dbo.gzy_user_member
-SET user_id = b.user_id,
-    com_id = b.com_id
-FROM dbo.gzy_user_member AS a
-    LEFT JOIN dbo.gzy_user_third AS b
-        ON a.mobile = b.mobile
-           AND a.brand_id = b.brand_id
-WHERE b.user_id IS NOT NULL;
+----更新会员人脸和企业
+--UPDATE dbo.gzy_user_member
+--SET user_id = b.user_id,
+--    com_id = b.com_id
+--FROM dbo.gzy_user_member AS a
+--    LEFT JOIN dbo.gzy_user_third AS b
+--        ON a.mobile = b.mobile
+--           AND a.brand_id = b.brand_id
+--WHERE b.user_id IS NOT NULL;
 
---更新第三方会员
-UPDATE dbo.gzy_user_third
-SET member_id = b.member_id
-FROM dbo.gzy_user_third AS a
-    LEFT JOIN dbo.gzy_user_member AS b
-        ON b.brand_id = a.brand_id
-           AND b.user_id = a.user_id;
+----更新第三方会员
+--UPDATE dbo.gzy_user_third
+--SET member_id = b.member_id
+--FROM dbo.gzy_user_third AS a
+--    LEFT JOIN dbo.gzy_user_member AS b
+--        ON b.brand_id = a.brand_id
+--           AND b.user_id = a.user_id;
